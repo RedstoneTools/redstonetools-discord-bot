c68971d974a466314aa5e84eca22a0808b2cff74
rugmj <rugmj@rugmj.dev>
2023-06-27 16:24:56 +0100

    adds trycatch in case an input breaks regex

--- src/commands/_faqAutocomplete.ts
+++ src/commands/_faqAutocomplete.ts
@@ -3,12 +3,18 @@ import { AutocompleteInteraction, CacheType } from 'discord.js';
 import { Faq } from '../database.js';
 
 async function autocomplete(interaction: AutocompleteInteraction<CacheType>) {
-	const results = await Faq.find({
-		name: new RegExp(`^${interaction.options.getFocused()}`, 'i'),
-	});
-	await interaction.respond(
-		results.map(choice => ({ name: choice.name, value: choice.name })),
-	);
+	try {
+		const results = await Faq.find({
+			name: new RegExp(`^${interaction.options.getFocused()}`, 'i'),
+		});
+		await interaction.respond(
+			results.map(choice => ({ name: choice.name, value: choice.name })),
+		);
+
+	} catch (err) {
+		console.error(err);
+		await interaction.respond([]);
+	}
 }
 
 export { autocomplete };
