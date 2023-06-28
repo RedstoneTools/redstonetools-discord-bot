ab3372d4ee64bff286bbcfba7f808461281dded9
rugmj <rugmj@rugmj.dev>
2023-06-28 18:55:58 +0100

    adds regex sanitisation

--- src/commands/_faqAutocomplete.ts
+++ src/commands/_faqAutocomplete.ts
@@ -3,10 +3,15 @@ import { AutocompleteInteraction, CacheType } from 'discord.js';
 import { Faq } from '../database.js';
 
 async function autocomplete(interaction: AutocompleteInteraction<CacheType>) {
+	const input = interaction.options.getFocused();
+
 	try {
 		const results = await Faq.find({
-			name: new RegExp(`^${interaction.options.getFocused()}`, 'i'),
+			name: new RegExp(`^${sanitiseRegexInput(input)}`, 'i'),
 		});
+
+
+		Faq.find
 		await interaction.respond(
 			results.map(choice => ({ name: choice.name, value: choice.name })),
 		);
@@ -15,6 +20,10 @@ async function autocomplete(interaction: AutocompleteInteraction<CacheType>) {
 		console.error(err);
 		await interaction.respond([]);
 	}
+}
+
+function sanitiseRegexInput(input: String) {
+	return input.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
 export { autocomplete };
